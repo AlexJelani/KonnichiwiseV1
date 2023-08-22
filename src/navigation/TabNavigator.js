db99a9ea97e27e493358fc59f4602dabaf69351e
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-08-22 21:51:20 +0900

    added TabStyle

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,65 +1,51 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Image } from "react-native";
-import * as Animatable from "react-native-animatable"; // Import Animatable
+import * as Animatable from "react-native-animatable";
 import Home from "../screens/Home";
 import Quiz from "../screens/Quiz";
 import icons from "../constants/icons";
+import { useNavigation } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
+  const navigation = useNavigation(); // Access the navigation prop
+
+  const tabBarIcon = (icon, focused, iconSize) => (
+    <Animatable.View
+      animation={focused ? "zoomIn" : undefined}
+      duration={300}
+    >
+      <Image
+        source={icon}
+        style={{
+          width: iconSize,
+          height: iconSize,
+          tintColor: focused ? "tomato" : "black",
+        }}
+      />
+    </Animatable.View>
+  );
+
   return (
     <Tab.Navigator
-      screenOptions={{
+      screenOptions={({ route, navigation }) => ({
         tabBarShowLabel: false,
         headerShown: false,
         tabBarActiveTintColor: "tomato",
         tabBarInactiveTintColor: "black",
-      }}
+        tabBarIcon: ({ focused }) => {
+          if (route.name === "Home") {
+            return tabBarIcon(icons.Home, focused, 60); // Home icon size
+          } else if (route.name === "Quiz") {
+            return tabBarIcon(icons.Quiz, focused, 35); // Adjust the Quiz icon size
+          }
+        },
+      })}
     >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Animatable.View
-              animation={focused ? "zoomIn" : undefined} // Apply animation only when focused
-              duration={300} // Set the duration of the animation
-            >
-              <Image
-                source={icons.Home}
-                style={{
-                  width: focused ? 60 : 50, // Adjust the width and height as needed
-                  height: focused ? 60 : 50,
-                  tintColor: focused ? "tomato" : "black",
-                }}
-              />
-            </Animatable.View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Quiz"
-        component={Quiz}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Animatable.View
-              animation={focused ? "zoomIn" : undefined} // Apply animation only when focused
-              duration={300} // Set the duration of the animation
-            >
-              <Image
-                source={icons.Quiz}
-                style={{
-                  width: focused ? 45 : 35, // Adjust the width and height as needed
-                  height: focused ? 45 : 35,
-                  tintColor: focused ? "tomato" : "black",
-                }}
-              />
-            </Animatable.View>
-          ),
-        }}
-      />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Quiz" component={Quiz} />
     </Tab.Navigator>
   );
 };
