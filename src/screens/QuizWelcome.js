264c5a94404d98e44e12a1ee0033ab46b2d218e8
Jelani K Alexander <94719282+AlexJelani@users.noreply.github.com>
2023-08-27 20:49:14 +0900

    added backgroundImage and updated quizwelcome ui

--- src/screens/QuizWelcome.js
+++ src/screens/QuizWelcome.js
@@ -1,71 +1,109 @@
-import {View,
-  Text,
-  Image,
+import {
+  View,
   TouchableOpacity,
-  StyleSheet,
-  } from "react-native"
+  StyleSheet, ImageBackground
+} from "react-native";
+import { Text } from "native-base";
+
 import React from "react";
 
 
-const QuizWelcome = ({ navigation }) =>{
+const QuizWelcome = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("../../assets/images/question-mark.png")} />
-      {/*PegandMarcoimage*/}
-      <View style={styles.subContainer}>
-        <Text style={styles.text}>Ready For your Written Test?</Text>
+      <View style={styles.textContainer}>
+        <Text fontSize="5xl" bold>
+          <Text style={styles.whiteText}>Welcome！</Text>
+          <Text style={styles.blackText}>Let's play the </Text>
+          <Text style={styles.whiteText}>KonnichiWise </Text>
+          <Text style={styles.blackText}>Quiz! </Text>
+
+        </Text>
       </View>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("QuizPage");
-        }}
-        style={styles.btn}
-      >
-        <Text style={styles.btnText}>Let's Begin</Text>
-      </TouchableOpacity>
+      <View style={styles.whiteSection}>
+
+        <ImageBackground
+          source={require('../../assets/images/quizbutton.png')} // Update with your image path
+          //photo by cottonbro studio
+          style={styles.backgroundImage}
+          resizeMode="cover" // You can adjust this property as needed
+        >
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("QuizPage");
+            }}
+            style={styles.btn}
+          >
+            <Text style={styles.btnText}>Let's Begin!</Text>
+          </TouchableOpacity>
+        </ImageBackground>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, // Set to 1 to take the full height of the screen
     backgroundColor: "red",
-    alignItem: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start", // Align content to the top
   },
-  image: {
-    width: "100%",
-    height: 350,
-    resizeMode: "contain",
-  },
-  subContainer: {
+  textContainer: {
+    flex: 1, // Set to 1 to take available space
+    paddingTop: 20,
     flexDirection: "row",
     justifyContent: "center",
-    alignItem: "center",
-    marginVertical: 20,
-    marginHorizontal: 20,
+    alignItems: "center",
+
+  },
+  whiteSection: {
+    //position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: "50%",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 56,
+    borderTopRightRadius: 56,
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 25,
     fontWeight: "bold",
     color: "#ffffff",
   },
+  blackText: {
+    fontFamily: "NightinTokyo",
+    color: "black",
+  },
+  whiteText: {
+    fontFamily: "NightinTokyo",
+    color: "white",
+  },
   btn: {
-    backgroundColor: "#fac782",
-    paddingHorizontal: 5,
-    paddingVertical: 15,
-    position: "relative",
-    borderRadius: 15,
+    backgroundColor: "red",
+    paddingHorizontal: 60,
+    paddingVertical: 60,
+    borderRadius: 25,
     marginHorizontal: 20,
     alignItems: "center",
+    elevation:10
   },
   btnText: {
     fontSize: 20,
     textAlign: "center",
     color: "#ffffff",
     letterSpacing: 1.1,
+    fontWeight:"bold"
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%", // Adjust these properties to fit your layout
+    height: "100%", // Adjust these properties to fit your layout
   },
 });
 
